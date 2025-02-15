32192004e29e12ee0bda2b5f451d1d1ba527cba3
Jamal <jamal.abdi19@gmail.com>
2025-02-15 16:28:17 -0500

    V1

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <img alt="Raptors" src="./assets/raptors.png" class="center">
+  <h1>Toronto Raptors </h1>
+  <img alt="Maple Leafs" src="./assets/maple-leafs.png" class="center">
+  <h1> Toronto Maple Leafs </h1>
+  <img alt="Blue Jays " src="./assets/bluejays.png" class="center">
+  <h1>Toronto Blue Jays </h1>
+</template>
+
+<script>
+console.log("Hello")
+</script>
+
+<style>
+h1 {text-align: center;}
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
+
